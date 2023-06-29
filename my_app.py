71ee070e5c5acf7a1dbf428df72e6cc8cb109100
ShinJaehun <gunsmoke.shin@gmail.com>
2023-06-29 22:19:26 +0900

    fix my_app and add gitignore

--- my_app.py
+++ my_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.write("""
 # Simple Stock Price App
-Shown are the stock closing price and volume of Google!
+Shown are the stock closing price and volume of **Google**!
 """)
 
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
@@ -12,8 +12,18 @@ tickerSymbol = 'GOOGL'
 #get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2023-5-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
+st.write("""
+### Closing Price
+""")
+
 st.line_chart(tickerDf.Close)
+
+
+st.write("""
+### Volume Price
+""")
+
 st.line_chart(tickerDf.Volume)
