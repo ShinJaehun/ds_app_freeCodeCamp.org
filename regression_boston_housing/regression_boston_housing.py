fa4309fc61348929a8fd0e5e7fab17eadf00c074
ShinJaehun <gunsmoke.shin@gmail.com>
2023-07-02 17:56:40 +0900

    fail to finish boston housing app because of shap library

---
+++ regression_boston_housing/regression_boston_housing.py
@@ -0,0 +1,107 @@
+import streamlit as st
+import pandas as pd
+import shap
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.ensemble import RandomForestRegressor
+
+st.write("""
+# Boston House Price Prediction App
+
+This app predicts the **Boston House Price**!
+""")
+st.write('---')
+
+# Loads the Boston House Price Dataset
+# boston = datasets.load_boston()
+# X = pd.DataFrame(boston.data, columns=boston.feature_names)
+# Y = pd.DataFrame(boston.target, columns=["MEDV"])
+
+column_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+boston = pd.read_csv('housing.csv', header=None, delimiter=r"\s+", names=column_names)
+# st.write(boston.head(5))
+
+# X = pd.DataFrame(boston[['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT']], columns=['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT'])
+# Y = pd.DataFrame(boston["MEDV"], columns=["MEDV"])
+
+X = boston[['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT']]
+Y = boston["MEDV"]
+
+st.write(X.dtypes)
+st.write(X.CRIM)
+# st.write(X.dtypes())
+# Sidebar
+# Header of Specify Input Parameters
+st.sidebar.header('Specify Input Parameters')
+
+def user_input_features():
+    CRIM = st.sidebar.slider('CRIM', float(X.CRIM.min()), float(X.CRIM.max()), float(X.CRIM.mean()))
+    # 아 쒸벌~ df의 key index 문제가 아니라 streamlit이 min()과 max()를 처리할때 발생하는 문제였네...
+    # https://github.com/dataprofessor/streamlit_freecodecamp/issues/1
+    ZN = st.sidebar.slider('ZN', float(X.ZN.min()), float(X.ZN.max()), float(X.ZN.mean()))
+    INDUS = st.sidebar.slider('INDUS', float(X.INDUS.min()), float(X.INDUS.max()), float(X.INDUS.mean()))
+    CHAS = st.sidebar.slider('CHAS', float(X.CHAS.min()), float(X.CHAS.max()), float(X.CHAS.mean()))
+    NOX = st.sidebar.slider('NOX', float(X.NOX.min()), float(X.NOX.max()), float(X.NOX.mean()))
+    RM = st.sidebar.slider('RM', float(X.RM.min()), float(X.RM.max()), float(X.RM.mean()))
+    AGE = st.sidebar.slider('AGE', float(X.AGE.min()), float(X.AGE.max()), float(X.AGE.mean()))
+    DIS = st.sidebar.slider('DIS', float(X.DIS.min()), float(X.DIS.max()), float(X.DIS.mean()))
+    RAD = st.sidebar.slider('RAD', float(X.RAD.min()), float(X.RAD.max()), float(X.RAD.mean()))
+    TAX = st.sidebar.slider('TAX', float(X.TAX.min()), float(X.TAX.max()), float(X.TAX.mean()))
+    PTRATIO = st.sidebar.slider('PTRATIO', float(X.PTRATIO.min()), float(X.PTRATIO.max()), float(X.PTRATIO.mean()))
+    B = st.sidebar.slider('B', float(X.B.min()), float(X.B.max()), float(X.B.mean()))
+    LSTAT = st.sidebar.slider('LSTAT', float(X.LSTAT.min()), float(X.LSTAT.max()), float(X.LSTAT.mean()))
+    data = {'CRIM': CRIM,
+            'ZN': ZN,
+            'INDUS': INDUS,
+            'CHAS': CHAS,
+            'NOX': NOX,
+            'RM': RM,
+            'AGE': AGE,
+            'DIS': DIS,
+            'RAD': RAD,
+            'TAX': TAX,
+            'PTRATIO': PTRATIO,
+            'B': B,
+            'LSTAT': LSTAT}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+# Main Panel
+
+# Print specified input parameters
+st.header('Specified Input parameters')
+st.write(df)
+st.write('---')
+
+# Build Regression Model
+model = RandomForestRegressor()
+model.fit(X, Y)
+# Apply Model to Make Prediction
+prediction = model.predict(df)
+
+st.header('Prediction of MEDV')
+st.write(prediction)
+st.write('---')
+
+# Explaining the model's predictions using SHAP values
+# https://github.com/slundberg/shap
+explainer = shap.TreeExplainer(model)
+shap_values = explainer.shap_values(X) # 여기서 np.bool deprecated라고 오류가 발생하는데 정확히 무슨 원인인지, 어떻게 해결할지 모르겠음
+
+# 여러가지 방법으로 해봤는데 shap 자체에 문제가 있는 듯!
+# import xgboost
+# model = xgboost.XGBRegressor().fit(X, Y)
+# explainer = shap.Explainer(model)
+# shap_values = explainer(X)
+
+st.header('Feature Importance')
+plt.title('Feature importance based on SHAP values')
+shap.summary_plot(shap_values, X)
+st.pyplot(bbox_inches='tight')
+st.write('---')
+
+plt.title('Feature importance based on SHAP values (Bar)')
+shap.summary_plot(shap_values, X, plot_type="bar")
+st.pyplot(bbox_inches='tight')
